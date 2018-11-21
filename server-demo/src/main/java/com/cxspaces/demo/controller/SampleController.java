b06407bb6cb9a82f9337985241130ef0e26ae4e6
bo.zhang <bo.zhang@51zouchuqu.com>
2018-11-22 01:10:03 +0800

    initial check in - able to store session in redis

---
+++ server-demo/src/main/java/com/cxspaces/demo/controller/SampleController.java
@@ -0,0 +1,56 @@
+package com.cxspaces.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+	@Autowired
+	private GreetingService service;
+	
+    @RequestMapping("/")
+    String home() {
+        return service.sayHello();
+    }
+    
+	@RequestMapping(value="/getSessionId")
+	@ResponseBody
+	public String getSessionId(HttpServletRequest request){
+		return service.getSessionId(request);
+	}
+}
+
+@Component
+class GreetingService {
+	public String getName() {
+		return System.getProperty("instance-name", "UNKNOWN");
+	}
+	
+	public String getSessionId(HttpServletRequest request){
+		
+		Object o = request.getSession().getAttribute("creator");
+		if(o == null){
+			o = getName() + ":" + request.getLocalPort();
+			request.getSession().setAttribute("creator", o);
+		}
+		
+		Integer count = 0;
+		Object countObj = request.getSession().getAttribute("count");
+		
+		if (count instanceof Integer && countObj != null)
+			count = (Integer)countObj;
+		count ++;
+		request.getSession().setAttribute("count", count);
+		
+		return "creator=" + o +  " sessionId=" + request.getSession().getId() +"<br/>" + "count=" + count;
+	}
+
+	public String sayHello() {
+		return "hello from service " + getName();
+	}
+}
